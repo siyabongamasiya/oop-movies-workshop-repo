e482b908f457c3dad0b3e227486a4c1e28d4bed8
siyabonga <siyabongakhanyile76@gmail.com>
2025-08-15 12:31:15 +0200

    feat:added do while

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-let sentence = "Hello world.I am Masiya"
+let sentence = "Hello world.I am Masiya";
 
 // for(let i=0 ; i < 6;i++){
 //     console.log(sentence[i])
 // }
 
-let listBook = ["harry porter","wong","zelda","dorra","blase","folis"]
+let listBook = ["harry porter", "wong", "zelda", "dorra", "blase", "folis"];
 
-// let i=0 
+// let i=0
 // for(; i < 6;){
 //     listBook[i][0] ="j"
 //     console.log(listBook[i][0])
 //     i++
 // }
-
 
 // for(letter of sentence){
 //     console.log(letter)
@@ -23,20 +22,31 @@ let listBook = ["harry porter","wong","zelda","dorra","blase","folis"]
 // }
 
 let marks = {
-    siya : 44,
-    msizi : 50,
-    sabelo : 60,
-    mpume : 70,
-    shantela : 88
-}
+  siya: 44,
+  msizi: 50,
+  sabelo: 60,
+  mpume: 70,
+  shantela: 88,
+};
 
 // for(name in marks){
 //     console.log(marks[name])
 // }
 
-let i = 0
-while(i < 6){
-    console.log(listBook[i])
-    i++
-}
+let i = 0;
+// while(i < 6){
+//     console.log(listBook[i])
+//     i++
+// }
 
+// let condition = true;
+// do {
+//   console.log("do while is running!!");
+//   condition = false;
+// } while (condition);
+
+let j = 10
+do {
+  console.log("another do while!!");
+  j++
+} while (j < 6);
