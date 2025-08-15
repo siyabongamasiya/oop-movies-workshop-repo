69d915583d40eac460ceb9caa202b66008aafc63
siyabonga <siyabongakhanyile76@gmail.com>
2025-08-15 11:57:13 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,42 @@
+let sentence = "Hello world.I am Masiya"
+
+// for(let i=0 ; i < 6;i++){
+//     console.log(sentence[i])
+// }
+
+let listBook = ["harry porter","wong","zelda","dorra","blase","folis"]
+
+// let i=0 
+// for(; i < 6;){
+//     listBook[i][0] ="j"
+//     console.log(listBook[i][0])
+//     i++
+// }
+
+
+// for(letter of sentence){
+//     console.log(letter)
+// }
+
+// for(book of listBook){
+//     console.log(book)
+// }
+
+let marks = {
+    siya : 44,
+    msizi : 50,
+    sabelo : 60,
+    mpume : 70,
+    shantela : 88
+}
+
+// for(name in marks){
+//     console.log(marks[name])
+// }
+
+let i = 0
+while(i < 6){
+    console.log(listBook[i])
+    i++
+}
+
---
+++ movies.js
@@ -0,0 +1,26 @@
+const movies = [
+  {
+    title: "avengers",
+    director: "Russo",
+    year: 2012,
+    genre: "Action",
+    rating: 9,
+    actors: ["robert","ruffalo","scarlet"],
+  },
+  {
+    title: "Batman",
+    director: "zack snyder",
+    year: 2015,
+    genre: "action",
+    rating: 7,
+    actors: ["jack","zao","pieter"],
+  },
+  {
+    title: "Doctor strange",
+    director: "Russo",
+    year: 2018,
+    genre: "action",
+    rating: 8,
+    actors: ["david","wong","parker"],
+  },
+];
